fe0f766602c756cbb320ad14303433ed12ca708f
Mikael <mikael>
2023-03-11 23:25:18 -0300

    cool

---
+++ math.js
@@ -0,0 +1,65 @@
+// dataSample as we go
+let dataSample = [];
+exports.cardPull = function(color){
+    dataSample.push(color);
+    if (dataSample.length > 200){
+        dataSample.shift();
+    }
+    return dataSample;
+}
+
+// calculte variance - this should be around 0.37-0.38 on bigger samples
+exports.calculateVariance = function(dataSample) {
+    // calculate mean
+    // calculate the sum of the squared differences between each data point and the mean
+    // calculate the variance
+    const mean = dataSample.reduce((a, b) => a + b, 0) / dataSample.length;
+    const sumOfSquaredDiffs = dataSample.reduce((a, b) => a + Math.pow((b - mean), 2), 0);
+    const variance = sumOfSquaredDiffs / dataSample.length;
+  
+    return variance;
+}
+
+exports.generateRandomNumber = function(){
+    // turns out that just doing it randomly gets you a 5-10% better result
+    // who would know
+    randomNumber = Math.floor(Math.random() * 16)
+    if(1 <= randomNumber <= 7){
+        return 1;
+    }
+    else if( 7 < randomNumber <= 14){
+        return 2;
+    }
+}
+
+// calculate win ratio
+// it would be better to put in a list with 1 for win and 0 for loss
+// calculate all the 1s / size
+// then shift when size > datasample lenght
+// should fix this asap
+let win = 0;
+let games = 0;
+exports.guessedRight = function(guess, result){
+    if (guess===result){
+        win+=1;
+    }
+    games+=1;
+
+    return (100*(win/games)).toFixed(2);
+}
+
+// this is just kinda calculate some kinda of frequency for 0 rolls.
+let weight = 0; 
+exports.tableWeight = function(color){
+    if (color===0){
+        weight = 0;
+    }
+    if (color===1){
+        weight+=1;
+    }
+    if (color===2){
+        weight-=1;
+    }
+
+    return weight
+}
---
+++ web.js
@@ -0,0 +1,90 @@
+const WebSocket = require('ws');
+const mathLib = require('./math')
+
+// blaze websocket endpoint
+const socket = new WebSocket("wss://api-v2.blaze.com/replication/?EIO=3&transport=websocket");
+
+// websocket on ping
+const pingInterval = setInterval(function() {
+    socket.send('2');
+    console.log('+ ping');
+  }, 25000);
+
+// websocket on open
+socket.onopen = function(event) {
+    console.log("+ connection opened");
+    // subscrition request to enter double_v2 chat/game
+    socket.send('421["cmd",{"id":"subscribe","payload":{"room":"double_v2"}}]');
+    console.log("+ sent subscription request to room double_v2")
+};
+
+
+// I REALLY NEED TO DO SOME REFACTORING... I KNOW
+let rollId = 0;
+let checkStatus = 0;
+let guess = -1;
+let hasGuessed = 'NO';
+socket.onmessage = function(event) {
+    // 
+    let time = new Date();
+    let currentHour = time.getHours()+":"+time.getMinutes()+":"+time.getSeconds();
+
+    // this if catches the pong messages provided by the server once with ping it.
+    if (event.data === '3') {
+        console.log('- pong');
+    }
+    else {
+        // try-catch to parse data
+        try {
+            // parses the data into a JSON format after the websocket 42 indentifier.
+            // i delete "bets" cus its a fuckin big ass blob of whatever information.
+            const parsedData = JSON.parse(event.data.substr(2));
+            delete parsedData[1].payload.bets;
+            const stringfiedData = JSON.stringify(parsedData);
+
+            // this throws away the live.bet.feed id and cleans output
+            if (parsedData[1].id === 'double.tick' && checkStatus != parsedData[1].payload.status){
+                checkStatus = parsedData[1].payload.status;
+                console.log(`status: \x1b[34m${checkStatus}\x1b[0m`);
+
+                if (checkStatus === 'waiting' && hasGuessed === 'NO') {
+                    rollId = parsedData[1].payload.status;
+                    guess = mathLib.generateRandomNumber();
+                    console.log('   guess:', guess);
+                    hasGuessed = 'YES';
+                }
+            }
+
+            // this will be true once per roll and its easier to use data
+            if ( (rollId != parsedData[1].payload.id) && (parsedData[1].payload.status === 'complete') ){
+                rollId = parsedData[1].payload.id;
+                let dataSampleVar = mathLib.cardPull(parsedData[1].payload.color);
+
+                console.log(`color: \x1b[32m${parsedData[1].payload.color}\x1b[0m roll: \x1b[32m${parsedData[1].payload.roll}\x1b[0m id: \x1b[32m${parsedData[1].payload.id}\x1b[0m - ${currentHour}`);
+                console.log('dataSample size:', dataSampleVar.length);
+                console.log('variance:', mathLib.calculateVariance(dataSampleVar));
+                console.log('table weight:', mathLib.tableWeight(parsedData[1].payload.color));
+                console.log(`win ratio: ${mathLib.guessedRight(guess, parsedData[1].payload.color)}%`);
+                hasGuessed = 'NO';
+            }
+        // treat websocket handshake messages || catch errors
+        } catch (error) {
+            if ( event.data.startsWith('4')|| event.data.startsWith('0') ){
+                console.log(event.data);
+            }
+            else{
+                console.log(error);
+            }
+        }
+    }
+};
+
+// websocket on error
+socket.onerror = function(error) {
+    console.log("Error:", error);
+};
+
+// websocket on close
+socket.onclose = function(event) {
+    console.log("[-] Connection closed");
+};
