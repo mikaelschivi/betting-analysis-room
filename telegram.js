2b0738b9e0cf2c2a3fa365eb0c87f20322aced36
real_batman42 <mikaelschivi@gmail.com>
2023-03-15 01:39:48 -0300

    tweaks

--- telegram.js
+++ telegram.js
@@ -1,32 +1,37 @@
 const fs = require('fs');
 const TelegramBot = require('node-telegram-bot-api');
 
-// grab token + chatid in file
+// grab token + chatid in file - could get chatid by api/getUpdates but easier this way
 const TOKEN = fs.readFileSync('./helper/token', 'utf8').trim();
 const CHATID = fs.readFileSync('./helper/chat_id', 'utf8').trim();
 
-// create bot
+// bot handler
 const bot = new TelegramBot(TOKEN, { polling: false });
 
-// function to send message to telegram channel
+// send message to telegram channel
 exports.sendSignal = function( guess ) {
+    let message = '';
     if (guess === 1) {
-        const message = `📢 JOGUE NO 🔴`;
+        message = `📢 JOGUE NO 🔴`;
         bot.sendMessage(CHATID, message);
-        console.log('signal:',message);
     }
-    if (guess === 2) {
-        const message = `📢 JOGUE NO ⚫️`;
+    else if (guess === 2) {
+        message = `📢 JOGUE NO ⚫️`;
         bot.sendMessage(CHATID, message);
-        console.log('signal:',message);
     }
+    return message;
 };
 
 // check signal i will eventually use this for martingale
-exports.checkSignal = function( guess, roll) {
+exports.checkSignal = function( guess, roll ) {
+    let message = '';
     if (guess === roll) {
-        const message = `✅ Win`;
+        message = `✅ Win`;
         bot.sendMessage(CHATID, message);
-        console.log('signal check:',message);
     }
+    else {
+        message = `❌ Loss`;
+        bot.sendMessage(CHATID, message);
+    }
+    return message
 };
